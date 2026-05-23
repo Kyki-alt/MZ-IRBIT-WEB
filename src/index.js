898bbb6f4763ae4f1e4a7f6c1d3318d7a995d8e3
Kykizi <lolkeklol098123321@gmail.com>
2026-05-24 03:48:59 +0500

    fix redirects copy for render linux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 import {
-  BrowserRouter,
+  HashRouter,
   Routes,
   Route
 } from 'react-router-dom'
@@ -17,7 +17,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
-<BrowserRouter>
+<HashRouter>
 
   <Routes>
 
@@ -43,6 +43,6 @@ root.render(
 
   </Routes>
 
-</BrowserRouter>
+</HashRouter>
   </React.StrictMode>
 )
