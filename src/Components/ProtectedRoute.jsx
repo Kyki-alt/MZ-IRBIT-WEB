487572df37bc05e33bb74eb1efcbd3a48f7e45c3
Kykizi <lolkeklol098123321@gmail.com>
2026-05-25 15:14:44 +0500

    fix redirects copy for render linux

--- src/Components/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute.jsx
@@ -1,12 +1,72 @@
+import { useEffect, useState } from 'react'
 import { Navigate } from 'react-router-dom'
+import axios from 'axios'
 
-export default function ProtectedRoute({ children }) {
+export default function ProtectedRoute({
+  children
+}) {
 
-  const token =
-    localStorage.getItem('adminToken')
+  const [isLoading, setIsLoading] =
+    useState(true)
 
-  if (!token) {
-    return <Navigate to="/admin-login" />
+  const [isAuth, setIsAuth] =
+    useState(false)
+
+  useEffect(() => {
+
+    const checkAuth = async () => {
+
+      const token =
+        localStorage.getItem(
+          'adminToken'
+        )
+
+      if (!token) {
+        setIsLoading(false)
+        return
+      }
+
+      try {
+
+        await axios.get(
+          'https://mz-irbit.onrender.com/admin/check',
+          {
+            headers: {
+              Authorization:
+                `Bearer ${token}`
+            }
+          }
+        )
+
+        setIsAuth(true)
+
+      } catch (err) {
+
+        localStorage.removeItem(
+          'adminToken'
+        )
+
+        setIsAuth(false)
+
+      } finally {
+
+        setIsLoading(false)
+
+      }
+    }
+
+    checkAuth()
+
+  }, [])
+
+  if (isLoading) {
+    return <h2>Проверка доступа...</h2>
+  }
+
+  if (!isAuth) {
+    return (
+      <Navigate to="/admin/login" />
+    )
   }
 
   return children
