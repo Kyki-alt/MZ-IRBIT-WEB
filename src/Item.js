f6e4f488e3abdd4bb1a9b806c892c22d21666791
Kykizi <lolkeklol098123321@gmail.com>
2026-05-26 23:59:05 +0500

    работа с формой

--- src/Item.js
+++ src/Item.js
@@ -1,65 +1,103 @@
-return (
-  <div className='item'>
+import React, { Component } from 'react'
 
-    <div className='item-image'>
-      <img
-        src={getImageUrl(item.img)}
-        alt=""
-      />
-    </div>
+export class Item extends Component {
 
-    <div className='item-info'>
+  render() {
 
-      <h2>{item.title}</h2>
+    const item = this.props.item
 
-      <b>{item.price}₽</b>
+    // Проверяем есть ли товар в корзине
+    const orderItem = this.props.orders.find(
+      el => el.id === item.id
+    )
 
-      {item.stock <= 0 ? (
+    const getImageUrl = (img) => {
+      if (!img) {
+        return '/img/placeholder.png'
+      }
 
-        <div className="out-of-stock">
-          Нет в наличии
+      if (img.startsWith('http')) {
+        return img
+      }
+
+      const clean = img.replace(/^undefined/, '')
+
+      // старые товары без uploads/
+      if (
+        !clean.startsWith('/uploads')
+      ) {
+        return `https://mz-irbit.onrender.com/uploads/products/${clean}`
+      }
+
+      return `https://mz-irbit.onrender.com${clean}`
+    }
+
+    return (
+    <div className='item'>
+
+        <div className='item-image'>
+          <img
+            src={getImageUrl(item.img)}
+            alt=""
+          />
         </div>
 
-      ) : !orderItem ? (
+        <div className='item-info'>
 
-        <div
-          className='add-to-cart'
-          onClick={() => this.props.onAdd(item)}
-        >
-          +
+          <h2>{item.title}</h2>
+
+          <b>{item.price}₽</b>
+
+          {item.stock <= 0 ? (
+
+            <div className="out-of-stock">
+              Нет в наличии
+            </div>
+
+          ) : !orderItem ? (
+
+            <div
+              className='add-to-cart'
+              onClick={() => this.props.onAdd(item)}
+            >
+              +
+            </div>
+
+          ) : (
+
+            <div className='card-quantity-controls'>
+
+              <button
+                className='card-quantity-btn'
+                onClick={() =>
+                  this.props.decreaseQuantity(item.id)
+                }
+              >
+                -
+              </button>
+
+              <span className='card-quantity-value'>
+                {orderItem.quantity}
+              </span>
+
+              <button
+                className='card-quantity-btn'
+                onClick={() =>
+                  this.props.increaseQuantity(item.id)
+                }
+              >
+                +
+              </button>
+
+            </div>
+
+          )}
+
         </div>
 
-      ) : (
+      </div>
+    )
+  }
+}
 
-        <div className='card-quantity-controls'>
-
-          <button
-            className='card-quantity-btn'
-            onClick={() =>
-              this.props.decreaseQuantity(item.id)
-            }
-          >
-            -
-          </button>
-
-          <span className='card-quantity-value'>
-            {orderItem.quantity}
-          </span>
-
-          <button
-            className='card-quantity-btn'
-            onClick={() =>
-              this.props.increaseQuantity(item.id)
-            }
-          >
-            +
-          </button>
-
-        </div>
-
-      )}
-
-    </div>
-
-  </div>
-)
+export default Item
