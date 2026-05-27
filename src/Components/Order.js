eab637f8ea650e1bd19f6d3b1875de72687240b9
Kykizi <lolkeklol098123321@gmail.com>
2026-05-28 01:52:53 +0500

    работа с формой

--- src/Components/Order.js
+++ src/Components/Order.js
@@ -3,7 +3,8 @@ import { FaTrash } from 'react-icons/fa'
 
 export class Order extends Component {
   render() {
-    const { item } = this.props
+
+    const item = this.props.item
 
     const getImageUrl = (img) => {
       if (!img) {
@@ -16,6 +17,7 @@ export class Order extends Component {
 
       const clean = img.replace(/^undefined/, '')
 
+      // старые товары без uploads/
       if (!clean.startsWith('/uploads')) {
         return `https://mz-irbit.onrender.com/uploads/products/${clean}`
       }
@@ -25,19 +27,19 @@ export class Order extends Component {
 
     return (
       <div className='cart-item'>
+
         <img
           src={getImageUrl(item.img)}
-          alt={item.title}
+          alt=""
         />
 
         <div className='cart-item-info'>
           <h2>{item.title}</h2>
 
-          <p>
-            {item.price}₽ × {item.quantity}
-          </p>
+          <p>{item.price}₽ × {item.quantity}</p>
 
           <div className='quantity-controls'>
+
             <button
               className='quantity-btn'
               onClick={() =>
@@ -59,15 +61,19 @@ export class Order extends Component {
             >
               +
             </button>
+
           </div>
         </div>
 
-        <FaTrash
-          className='delete-icon'
-          onClick={() =>
-            this.props.onDelete(item.id)
-          }
-        />
+        <div className='cart-item-right'>
+          <FaTrash
+            className='delete-icon'
+            onClick={() =>
+              this.props.onDelete(item.id)
+            }
+          />
+        </div>
+
       </div>
     )
   }
