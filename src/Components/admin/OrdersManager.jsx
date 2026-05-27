7b1f0134eb163e26333a8ce28b8e71e3965aac2f
Kykizi <lolkeklol098123321@gmail.com>
2026-05-27 19:55:10 +0500

    работа с формой

--- src/Components/admin/OrdersManager.jsx
+++ src/Components/admin/OrdersManager.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import { Fragment, useEffect, useMemo, useState } from 'react'
 import './OrdersManager.css'
 
 export default function OrdersManager() {
